744d3161c070535d3874f56ee169ca0473cf48e2
Grayson Pike <graysonpike@gmail.com>
2015-02-21 20:08:03 -0600

    Added direction

--- core/src/com/entities/Player.java
+++ core/src/com/entities/Player.java
@@ -7,43 +7,83 @@ import com.physics.Hitbox;
 
 public class Player extends Entity{
 	
-	public Animation a_idle;
-	public Animation a_walking;
+	public Animation a_idle_right;
+	public Animation a_idle_left;
+	public Animation a_walking_right;
+	public Animation a_walking_left;
 	
-	public Player(){
-		super(0, 0);
-		a_idle = new Animation(1);
-		a_idle.addFrame(new Texture("entities/player/stand.png"), 1);
-								
-		a_walking = new Animation(2);
-		a_walking.addFrame(new Texture("entities/player/walk1.png"), 20);
-		a_walking.addFrame(new Texture("entities/player/walk2.png"), 20);
-		
-		hitbox = new Hitbox(x, y, getWidth(), getHeight());
-	}
+	public int speed = 5;
+	
+	public boolean moving = false;
+	public boolean heading = true;
 	
 	public Player(float x, float y){
-		super(x, y);
-		a_idle = new Animation(1);
-		a_idle.addFrame(new Texture("entities/player/stand.png"), 1);
-								
-		a_walking = new Animation(2);
-		a_walking.addFrame(new Texture("entities/player/walk1.png"), 20);
-		a_walking.addFrame(new Texture("entities/player/walk2.png"), 20);
+		super(y, x);
+		a_idle_right = new Animation(1);
+		a_idle_right.addFrame(new Texture("entities/player/stand_right.png"), 1);
+		
+		a_idle_left = new Animation(1);
+		a_idle_left.addFrame(new Texture("entities/player/stand_left.png"), 1);
+		
+		a_walking_right = new Animation(2);
+		a_walking_right.addFrame(new Texture("entities/player/walk1_right.png"), 20);
+		a_walking_right.addFrame(new Texture("entities/player/walk2_right.png"), 20);
+		
+		a_walking_left = new Animation(2);
+		a_walking_left.addFrame(new Texture("entities/player/walk1_left.png"), 20);
+		a_walking_left.addFrame(new Texture("entities/player/walk2_left.png"), 20);
 		
 		hitbox = new Hitbox(x, y, getWidth(), getHeight());
 	}
 	
 	public void render(SpriteBatch batch){
-		a_walking.render(batch, x, y);
+		if(moving){
+			if(heading){
+				a_walking_right.render(batch, x, y);
+			}
+			else{
+				a_walking_left.render(batch, x, y);
+			}
+			
+		}
+		else{
+			if(heading){
+				a_idle_right.render(batch, x, y);
+			}
+			else{
+				a_idle_left.render(batch, x, y);
+			}
+		}
+		moving = false;
 	}
 	
 	public int getWidth(){
-		return a_idle.frames[0].getWidth();
+		return a_idle_right.frames[0].getWidth();
 	}
 	
 	public int getHeight(){
-		return a_idle.frames[0].getHeight();
+		return a_idle_right.frames[0].getHeight();
+	}
+	
+	public void up(){
+		y += speed;
+		moving = true;
+	}
+	
+	public void down(){
+		y -= speed;
+		moving = true;
+	}
+	
+	public void left(){
+		x -= speed;
+		moving = true;
+		heading = false;
+	}
+	public void right(){
+		x += speed;
+		moving = true;
+		heading = true;
 	}
 	
 }
--- core/src/com/game/Sneak.java
+++ core/src/com/game/Sneak.java
@@ -2,6 +2,7 @@ package com.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,11 +16,31 @@ public class Sneak extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		guy = new Player();
+		guy = new Player(100, 100);
+	}
+	
+	public void update(){
+		
+		if(Gdx.input.isKeyPressed(Input.Keys.W)){
+			guy.up();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.A)){
+			guy.left();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.S)){
+			guy.down();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.D)){
+			guy.right();
+		}
+		
 	}
 
 	@Override
 	public void render () {
+		
+		update();
+		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
